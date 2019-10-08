e5175a68630543707bffac566bdbaa10b2d75067
kaaspaa <kasper12.01@wp.pl>
2019-10-08 20:30:28 +0200

    added tasks for zad3

---
+++ zaj1/zad3.py
@@ -0,0 +1,15 @@
+#a)
+a) Stwórz plik mojefunkcje.r i otwórz go w R Studio (plik będziemy edytować w lewym górnym oknie w R Studio).
+
+#b)
+b) Wpisz do tego pliku kod funkcji sumującej dwie liczy. Komendy wpisuj w osobnych linijkach (R nie akceptuje jednolinijkowych programów).
+sumuj <- function(a,b) {
+ s <-a+b
+return(s)
+}
+
+#c)
+c) Zapisz plik i wykonaj go klikając na guzik „Run”. Funkcja zostanie załadowane do Workspace.
+
+#d)
+d) Przetestuj działanie funkcji wpisując sumuj (12,30) w konsoli. Czy funkcja działa dla wektorów?
