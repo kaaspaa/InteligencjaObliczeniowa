a77efdde4c23fab28430254ad59a6e99d5c66795
kaaspaa <kasper12.01@wp.pl>
2019-10-08 20:36:30 +0200

    added zad4 tasks and optimalizied zad3 tasks

---
+++ zaj1/zad4.py
@@ -0,0 +1,23 @@
+"""
+a) Nazwa funkcji: losuj
+Wejście: dwie liczby a,b
+Wyjście: funkcja zwraca losową liczbę z przedziału [a,b] (lub [b,a])
+"""
+"""
+
+b) Nazwa funkcji: standaryzuj
+Wejście: wektor liczb v
+Wyjście: funkcja zwraca wektor z liczbami ustandaryzowanymi (https://pl.wikipedia.org/wiki/Standaryzacja_(statystyka))
+"""
+"""
+
+c) Nazwa funkcji: normalizuj
+Wejście: wektor liczb v
+Wyjście: funkcja zwraca wektor z liczbami znormalizowanymi metodą min-max
+"""
+"""
+
+d) Nazwa funkcji: wyszukaj
+Wejście: wektor liczb v, liczba x 
+Wyjście: funkcja zwraca liczbę liczb z wektora v, które są większe niż x(pouczające będzie wykonanie tej funkcji w dwóch wersjach – z wykorzystaniem pętli for, oraz bez niej, za pomocą sprytnych komend).
+"""
