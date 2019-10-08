2435ea46be00d519c6fbd556e622c3d1ee7496d5
kaaspaa <kasper12.01@wp.pl>
2019-10-08 16:28:42 +0200

    added zad2.py file with tasks and completed one

---
+++ zaj1/zad2.py
@@ -0,0 +1,31 @@
+import os
+
+#a)
+print("a)Sprawdź ścieżkę do „working directory” i ewentualnie ustaw ją na własną.")
+cwd = os.getcwd()
+print("working directory: ",cwd,"\n")
+
+#b)
+print("b)Pobierz ze strony i otwórz plik osoby.csv ze wskazaniem, że kolumny mają nagłówki, a separatorem jest przecinek. Następnie wyświetl tabelę danych.")
+
+#c)
+print("c)Wyświetl same imiona.")
+
+#d)
+print("d)Wyświetl tylko dane kobiet.")
+
+#e)
+print("e)Wyświetl dane mężczyzn starszych niż 50 lat i zapisz je w osobnym pliku osoby2.csv.")
+
+#f)
+print("f)Za pomocą odpowiedniej komendy podaj średni wiek osób.")
+
+#g)
+print("g)Dodaj kolumnę o nazwie „wyplata” i uzupełnij ją losowymi liczbami z zakresu (2000,5000)")
+
+#h)
+print("h)Dodaj nowy rekord do tabeli: Jan Kowalski, wiek 30, wypłata 3000.")
+
+#i)
+print("i)Zapisz zmodyfikowaną tabelę do pliku osoby3.csv.")
+
