9e3ab64be6bc25f1e7b0b035b8bde7151e895ff2
kaaspaa <kasper12.01@wp.pl>
2019-10-08 14:31:33 +0200

    added zaj1 dir, added zad1.py and put questions there and one answer

---
+++ zaj1/zad1.py
@@ -0,0 +1,17 @@
+#a)
+print("a) Oblicz ile to jest 45*678")
+print("odp = ", 45*678)
+#b)
+print("b) Wczytaj pod zmienne dwa wektory: x=(7; 4; 2; 0; 9) oraz y=(2; 1; 5; 3; 3).")
+
+#c)
+print("c)Oblicz za pomocą jednej operacji wektor, którego współrzędne są sumami współrzędnych wektorów x i y. Wyświetl ten wektor.")
+
+#d)
+print("d)Oblicz w podobny sposób iloczyn y współrzędnych wektorów.")
+
+#e)
+print("e)Oblicz za pomocą mnożenia macierzowego iloczyn skalarny wektorów (wynik to liczba będąca sumą iloczynów współrzędnych).")
+
+#f)
+print("f)Oblicz iloczyn macierzy korzystając z mnożenia macierzowego.")
