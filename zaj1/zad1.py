1321d388e3fbda2fb8f810e641f6a37698df6ace
kaaspaa <kasper12.01@wp.pl>
2019-10-08 14:48:14 +0200

    added another tasks

--- zaj1/zad1.py
+++ zaj1/zad1.py
@@ -1,6 +1,7 @@
 #a)
 print("a) Oblicz ile to jest 45*678")
-print("odp = ", 45*678)
+print("odp = ", 45*678,"\n")
+
 #b)
 print("b) Wczytaj pod zmienne dwa wektory: x=(7; 4; 2; 0; 9) oraz y=(2; 1; 5; 3; 3).")
 
@@ -15,3 +16,21 @@ print("e)Oblicz za pomocą mnożenia macierzowego iloczyn skalarny wektorów (wy
 
 #f)
 print("f)Oblicz iloczyn macierzy korzystając z mnożenia macierzowego.")
+print(" _     _   _     _")
+print(" |0,2,1|   |9,8,7|")
+print(" |1,6,4|   |1,2,7|")
+print(" |5,0,3|   |4,9,2|")
+print(" ‾     ‾   ‾     ‾")
+
+#g)
+print("g)Wygeneruj za pomocą odpowiedniej komendy wektor z liczbami od 1 do 100.")
+
+#h)
+print("(h)Oblicz sumę, średnią, odchylenie standardowe tych liczb za pomocą odpowiednich komend.")
+
+#i)
+print("i)Wygeneruj za pomocą odpowiedniej komendy wektor z 20 losowymi liczbami od 0 do 50.")
+
+#j)
+print("j) Oblicz min i max z tych losowych liczb za pomocą odpowiednich komend.")
+
