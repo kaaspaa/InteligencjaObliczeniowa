ca3134c449f66061790ebe9b8a21a5a6903c69f0
kaaspaa <klaskowski@linux-p39-5.inf.ug.edu.pl>
2019-11-06 13:29:25 +0100

    added answet to zad1 task

--- zaj5/zad1.py
+++ zaj5/zad1.py
@@ -1,3 +1,21 @@
 import pandas as pd
+from sklearn import datasets
 
+def myPredictRow(sl, sw, pl, pw):
+    if pw < 0.5 :
+        return "Iris-setosa"
+    elif pl < 5:
+        return "Iris-versicolor"
+    else:
+        return "Iris-virginica"
 
+df = pd.read_csv("iris.csv")
+eval = 0
+for i in range(df.shape[0]):
+    if df.loc[i]["class"] == myPredictRow(df.loc[i]["sepallength"], df.loc[i]["sepalwidth"], df.loc[i]["petallength"], df.loc[i]["petalwidth"]):
+        eval += 1
+        print(df.loc[i]["class"])
+print("eval = " + str(eval) + "/" + str(150))
+#iris = datasets.load_iris()
+#digits = datasets.load_digits()
+#print(digits.data)
