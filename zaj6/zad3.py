e33db2ecc127677a1e024c794534769fa6b8cead
kaaspaa <klaskowski@linux-p39-5.inf.ug.edu.pl>
2019-11-20 12:56:58 +0100

    added matrixes, and change shapes in plot

--- zaj6/zad3.py
+++ zaj6/zad3.py
@@ -1,4 +1,6 @@
+import classifier as classifier
 import pandas as pd
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
@@ -11,32 +13,45 @@ import matplotlib.pyplot as plt
 sns.set()
 diabetes = pd.read_csv("diabetes.csv")
 X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'class'], diabetes['class'],
-                                                    stratify=diabetes['class'], test_size=33, train_size=67)
+                                                    stratify=diabetes['class'], test_size=0.33, train_size=0.67)
 neighbors_settings = [3,5,11]
 for n_neighbors in neighbors_settings:
     # build the model
     knn = KNeighborsClassifier(n_neighbors=n_neighbors)
     knn.fit(X_train, y_train)
     # record test set accuracy
+
+    y_pred = knn.predict(X_test)
     knn_score = knn.score(X_test, y_test)
-    plt.plot(n_neighbors, knn_score, 'bo', label=("knn " + str(n_neighbors)))
+    plt.plot(n_neighbors, knn_score, 'bs', label=("knn " + str(n_neighbors)))
+    cm = confusion_matrix(y_test, y_pred)
+    print("matrix dla knn " + str(n_neighbors) + " = ")
+    print(cm)
 
 # Naive Bayes
 X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'class'], diabetes['class'],
-                                                    stratify=diabetes['class'], test_size=33, train_size=67)
+                                                    stratify=diabetes['class'], test_size=0.33, train_size=0.67)
 gnb = GaussianNB()
 gnb.fit(X_train, y_train)
 y_predict = gnb.predict(X_test)
 gnb_score = gnb.score(X_test, y_test)
-plt.plot(7, gnb_score, 'ro', label="Gaussian Naive Byes")
+plt.plot(7, gnb_score, 'rs', label="Gaussian Naive Byes")
+y_pred = gnb.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print("Naive Bayes = ")
+print(cm)
 
 # Decision Tree Classifier
 X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'class'], diabetes['class'],
-                                                    stratify=diabetes['class'], test_size=33, train_size=67)
+                                                    stratify=diabetes['class'], test_size=0.33, train_size=0.67)
 dtc = DecisionTreeClassifier()
 dtc.fit(X_train, y_train)
 dtc_score = dtc.score(X_test, y_test)
-plt.plot(9, dtc_score, 'go', label="Decision Tree")
+plt.plot(9, dtc_score, 'gs', label="Decision Tree")
+y_pred = dtc.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print("Decision Tree = ")
+print(cm)
 
 # wykres
 plt.axis([0,12,0,1])
